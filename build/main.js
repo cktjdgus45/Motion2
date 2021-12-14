07794efee77fe2f2f0267d5fcca786e2f49f770a
pooh <cktjdgus45@naver.com>
2021-12-14 16:35:31 +0900

    add form pop up

--- build/main.js
+++ build/main.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('test change');
+const headerBtns = document.querySelector('.header-btns');
+const mediaPopup = document.querySelector('.media-form');
+const textPopup = document.querySelector('.text-form');
+const handleClick = (event) => {
+    const target = event.target;
+    const text = target.innerText;
+    console.log(text);
+    text === "image" || text === "video" ? mediaPopup === null || mediaPopup === void 0 ? void 0 : mediaPopup.classList.toggle('hide') :
+        text === "note" || text === "task" ? textPopup === null || textPopup === void 0 ? void 0 : textPopup.classList.toggle('hide') :
+            console.log('');
+};
+headerBtns === null || headerBtns === void 0 ? void 0 : headerBtns.addEventListener('click', handleClick);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log('test change')
+const headerBtns = document.querySelector('.header-btns');
+const mediaPopup = document.querySelector('.media-form');
+const textPopup = document.querySelector('.text-form');
+
+const handleClick = (event: Event) => {
+    const target = event.target as HTMLElement;
+    const text = target.innerText;
+    console.log(text);
+    text === "image" || text === "video" ? mediaPopup?.classList.toggle('hide') :
+        text === "note" || text === "task" ? textPopup?.classList.toggle('hide') :
+            console.log('');
+}
+headerBtns?.addEventListener('click', handleClick);
