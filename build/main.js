03772ce4b9b960c4c49e2c1deea14c2f5b416836
pooh <cktjdgus45@naver.com>
2021-12-14 17:24:53 +0900

    get form value

--- build/main.js
+++ build/main.js
@@ -1,13 +1,36 @@
 "use strict";
 const headerBtns = document.querySelector('.header-btns');
-const mediaPopup = document.querySelector('.media-form');
-const textPopup = document.querySelector('.text-form');
+const mediaForm = document.querySelector('.media-form');
+const textForm = document.querySelector('.text-form');
 const handleClick = (event) => {
     const target = event.target;
     const text = target.innerText;
     console.log(text);
-    text === "image" || text === "video" ? mediaPopup === null || mediaPopup === void 0 ? void 0 : mediaPopup.classList.toggle('hide') :
-        text === "note" || text === "task" ? textPopup === null || textPopup === void 0 ? void 0 : textPopup.classList.toggle('hide') :
-            console.log('');
+    text === "image" || text === "video" ? mediaForm === null || mediaForm === void 0 ? void 0 : mediaForm.classList.toggle('hide') :
+        console.log('');
+    text === "note" || text === "task" ? textForm === null || textForm === void 0 ? void 0 : textForm.classList.toggle('hide') :
+        console.log('');
+};
+const handleMediaSubmit = (event) => {
+    event.preventDefault();
+    const title = mediaForm === null || mediaForm === void 0 ? void 0 : mediaForm.querySelector('.media-form-title');
+    const url = mediaForm === null || mediaForm === void 0 ? void 0 : mediaForm.querySelector('.media-form-url');
+    console.dir(title.value);
+    console.dir(url.value);
+    title.value = "";
+    url.value = "";
+    mediaForm === null || mediaForm === void 0 ? void 0 : mediaForm.classList.add('hide');
+};
+const handleTextSubmit = (event) => {
+    event.preventDefault();
+    const title = textForm === null || textForm === void 0 ? void 0 : textForm.querySelector('.text-form-title');
+    const body = textForm === null || textForm === void 0 ? void 0 : textForm.querySelector('.text-form-body');
+    console.dir(title.value);
+    console.dir(body.value);
+    title.value = "";
+    body.value = "";
+    textForm === null || textForm === void 0 ? void 0 : textForm.classList.add('hide');
 };
 headerBtns === null || headerBtns === void 0 ? void 0 : headerBtns.addEventListener('click', handleClick);
+mediaForm === null || mediaForm === void 0 ? void 0 : mediaForm.addEventListener('submit', handleMediaSubmit);
+textForm === null || textForm === void 0 ? void 0 : textForm.addEventListener('submit', handleTextSubmit);
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,38 @@
 const headerBtns = document.querySelector('.header-btns');
-const mediaPopup = document.querySelector('.media-form');
-const textPopup = document.querySelector('.text-form');
+const mediaForm = document.querySelector('.media-form');
+const textForm = document.querySelector('.text-form');
 
 const handleClick = (event: Event) => {
     const target = event.target as HTMLElement;
     const text = target.innerText;
     console.log(text);
-    text === "image" || text === "video" ? mediaPopup?.classList.toggle('hide') :
-        text === "note" || text === "task" ? textPopup?.classList.toggle('hide') :
-            console.log('');
+    text === "image" || text === "video" ? mediaForm?.classList.toggle('hide') :
+        console.log('')
+    text === "note" || text === "task" ? textForm?.classList.toggle('hide') :
+        console.log('');
 }
+
+const handleMediaSubmit = (event: Event) => {
+    event.preventDefault();
+    const title = mediaForm?.querySelector('.media-form-title') as HTMLInputElement;
+    const url = mediaForm?.querySelector('.media-form-url') as HTMLInputElement;
+    console.dir(title.value)
+    console.dir(url.value)
+    title.value = "";
+    url.value = "";
+    mediaForm?.classList.add('hide');
+}
+const handleTextSubmit = (event: Event) => {
+    event.preventDefault();
+    const title = textForm?.querySelector('.text-form-title') as HTMLInputElement;
+    const body = textForm?.querySelector('.text-form-body') as HTMLInputElement;
+    console.dir(title.value)
+    console.dir(body.value)
+    title.value = "";
+    body.value = "";
+    textForm?.classList.add('hide');
+}
+
 headerBtns?.addEventListener('click', handleClick);
+mediaForm?.addEventListener('submit', handleMediaSubmit);
+textForm?.addEventListener('submit', handleTextSubmit);
